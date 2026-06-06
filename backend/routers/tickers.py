6eda30a32e362d1146c79f1d93deb78ff1856cb5
aarolehtoaho <aaro.lehtoaho.al@gmail.com>
2026-06-06 17:57:55 +0300

    Add routers for /tickers POST, GET and DELETE

--- backend/routers/tickers.py
+++ backend/routers/tickers.py
@@ -1,9 +1,16 @@
 from fastapi import APIRouter, Query
+from pydantic import BaseModel
 from backend.services.fetcher import search_tickers
 from backend.database import Database
 
 router = APIRouter()
 db = Database()
+
+class TickerItem(BaseModel):
+    symbol: str
+    name: str
+    exchange: str
+    position: int
 
 @router.get("/search")
 async def search(q: str = Query(min_length=1)) -> list[dict]:
@@ -15,3 +22,23 @@ async def search(q: str = Query(min_length=1)) -> list[dict]:
     db.cache_ticker_search(q, results)
 
     return results
+
+@router.get("/")
+async def list_tickers() -> list[dict]:
+    return db.list_tickers()
+
+@router.post("/")
+async def add_ticker(ticker_data: TickerItem):
+    ticker = {
+        "symbol": ticker_data.symbol, 
+        "name": ticker_data.name, 
+        "exchange": ticker_data.exchange
+    }
+    
+    db.add_ticker(ticker, ticker_data.position)
+    return {"message": f"Ticker {ticker_data.symbol} added to watchlist."}
+
+@router.delete("/{symbol}")
+async def remove_ticker(symbol: str):
+    db.remove_ticker(symbol)
+    return {"message": f"Ticker {symbol} removed from watchlist."}
